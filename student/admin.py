9a349360dbea2943c2497cdab9992e3c09713917
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2023-08-27 21:48:31 +0300

    Student -> classroom, AttendanceRecord, Student Grade

--- student/admin.py
+++ student/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from student.models import StudentProfile
+from student.models import StudentProfile,StudentClassroom,StudentGrade,AttendanceRecord
 # Register your models here.
 
 admin.site.register(StudentProfile)
+admin.site.register(StudentClassroom)
+admin.site.register(StudentGrade)
+admin.site.register(AttendanceRecord)
+
--- student/models.py
+++ student/models.py
@@ -1,8 +1,13 @@
 from django.db import models
-from autoslug import AutoSlugField
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
+from autoslug import AutoSlugField
+
+# Models
 from account.models import User
 from classroom.models import Classroom
+
+from datetime import date
 
 # Create your models here.
 
@@ -27,7 +32,49 @@ class StudentProfile(models.Model):
             'student:student_profile_overview',
             kwargs={'user_slug': self.slug}
         )
-
+    
+    def age(self):
+        if self.birth_date:
+            today = date.today()
+            age = today.year - self.birth_date.year - ((today.month, today.day) < (self.birth_date.month, self.birth_date.day))
+            return age
+        return None
     def get_absolute_url_edit(self):
         return reverse('student:student_profile_edit', kwargs={'user_slug': self.slug})
+    
+
+class StudentGrade(models.Model):
+    student = models.ForeignKey(StudentProfile, on_delete=models.CASCADE, related_name='classrooms')
+    classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE, related_name='students')
+    grade = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
+    exam_name = models.CharField(max_length=100)
+    date = models.DateField()
+    def __str__(self):
+        return f"{self.student.user.first_name} {self.student.user.last_name} - {self.exam_name}"
+    
+class AttendanceRecord(models.Model):
+    STATUS_CHOICES = [
+        ('P', 'Present'),
+        ('A', 'Absent'),
+    ]
+
+    date = models.DateField()
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+    notes = models.TextField(blank=True, null=True)
+
+class StudentClassroom(models.Model):
+    student = models.ForeignKey(StudentProfile, on_delete=models.CASCADE)
+    classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE)
+    attendance_records = models.ManyToManyField(AttendanceRecord)
+    enrollment_date = models.DateField(auto_now_add=True)
+    notes = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.student.user.first_name} {self.student.user.last_name} - {self.classroom.name}"
+    
+    # def get_absolute_url(self):
+    #     return reverse('student:student_classroom_detail', kwargs={'pk': self.pk})
+    
+
+
 
