fdec110a461d312ba18e0928b2ac205e5cf4c6ce
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2023-09-14 18:27:33 +0300

    remove:some comments

--- config/settings.py
+++ config/settings.py
@@ -160,7 +160,7 @@ AUTH_USER_MODEL = 'account.User'
 
 LOGIN_ADMIN_RESTRICTED = True
 
-# Default primary key field type
+
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
