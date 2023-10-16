d45610dc85ac28e18708f73e2a8dea35d5669cab
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2023-10-16 17:50:47 +0300

    update: blog_home for last 6 blogs

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
+# Models
+from blog.models import BlogPost
 # Create your views here.
 def blog_home(request):
-    return render(request, 'blog/blog_home.html', {})
+    posts = BlogPost.objects.all()[:6]  # get the last five published posts
+    post_1 = [posts[0], posts[1], posts[2]]
+    post_2 = [posts[3], posts[4], posts[5]]
+    context = dict(
+        post_1=post_1,
+        post_2=post_2,
+        posts=posts
+    )
+    return render(request, 'blog/blog_home.html', context)    
