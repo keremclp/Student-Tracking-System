48d7c21fd2495e7d69d824b4ff1e82f47fdec220
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2023-10-19 13:14:41 +0300

    add: blog creation functionality

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from blog.views import blog_home
+from blog.views import blog_home,create_blog_post_view
 app_name = 'blog'
 
 urlpatterns = [
     path('blog-home/', blog_home , name='blog_home'),
+    path('create-blog/', create_blog_post_view , name='create_blog'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+import json
+from django.shortcuts import redirect, render
+from django.contrib import messages
 
 # Models
-from blog.models import BlogPost
+from blog.models import BlogPost, Tag
+
+# Forms
+from blog.forms import BlogPostModelForm
 
 from django.contrib.auth.decorators import login_required
 
@@ -17,3 +22,31 @@ def blog_home(request):
         posts=posts
     )
     return render(request, 'blog/blog_home.html', context)    
+
+def create_blog_post_view(request):
+    title = "Yeni Blog Post :"
+    form = BlogPostModelForm()
+    context = dict(
+        title=title,
+        form=form
+    )
+
+    if request.method == 'POST':
+        form = BlogPostModelForm(request.POST or None, request.FILES or None)
+        if form.is_valid():
+            print("Valid oldu...")
+            f = form.save(commit=False)
+            print(form.cleaned_data)
+            f.user = request.user
+            f.save()
+            tags = json.loads(form.cleaned_data.get('tag'))
+            for item in tags:
+                tag_item, created = Tag.objects.get_or_create(title=item.get('value'))
+                tag_item.title = tag_item.title.lower() 
+                f.tag.isActive = True
+                tag_item.save()
+                f.tag.add(tag_item)
+            messages.success(request, "Blog kaydedildi")
+            return redirect('blog:blog_home')
+
+    return render(request, 'blog/create_blog.html', context)
