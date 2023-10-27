9bc156ae3590297159c1cb832f5b73ecb36d03bd
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2023-10-27 19:42:34 +0300

    fix: birth_date issue

--- student/forms.py
+++ student/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import StudentProfile
 from datetime import datetime
 
+
 class DateInput(forms.DateInput):
     input_type = 'date'
     format = '%d.%m.%Y'  # Format used in the form input
@@ -11,8 +12,9 @@ class StudentProfileModelForm(forms.ModelForm):
     first_name = forms.CharField(max_length=30, required=True)
     last_name = forms.CharField(max_length=30, required=True)
     profile_image = forms.ImageField(required=False)
-    birth_date = forms.DateField(widget=DateInput(format='%d.%m.%Y'), required=False)
-    
+    birth_date = forms.DateField(
+        widget=DateInput(format='%Y-%m-%d'), required=False)
+
     class Meta:
         model = StudentProfile
         fields = [
@@ -23,6 +25,7 @@ class StudentProfileModelForm(forms.ModelForm):
             'phone_number',
             'address',
         ]
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # Pre-fill the birth date field if the instance has a birth date
@@ -31,5 +34,3 @@ class StudentProfileModelForm(forms.ModelForm):
 
 
 # TODO:birth_date tarafında ne zaman edit yapmak istesek birth_day girilmesi gerekiyor, düzeltilmesi lazım
-
-    
